5511194419a3b1efb6d8aa3b58ed4249eb56d758
rafakwas <ralphigi@gmail.com>
2020-10-20 11:25:30 +0200

    optimize imports

--- src/main/java/com/example/demo/entity/Document.java
+++ src/main/java/com/example/demo/entity/Document.java
@@ -1,8 +1,13 @@
 package com.example.demo.entity;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Getter
